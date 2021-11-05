d4d16aebca3562f4d5aae090be70c4678713b4dd
Miguel <mbarrientos@morangesoft.es>
2021-11-05 16:24:39 -0500

    qwd4wqd4

---
+++ render-tag.php
@@ -0,0 +1,24 @@
+<?php
+<html lang='es-pe'>
+<meta name='og:image' content='https://morangesoft.es/wp-content/uploads/2021/05/Recurso-2-1024x436.png' />
+<meta name='twitter:image' content='https://morangesoft.es/wp-content/uploads/2021/05/Recurso-2-1024x436.png' />
+<title data-react-helmet='true'>Morangesoft</title>
+<link rel='canonical' href='HOME - Morangesoft'/>
+<meta name='og:type' content='Morangesoft' />
+<meta name='og:title' content='Morangesoft' />
+<meta name='og:url' content='HOME - Morangesoft' />
+<meta name='og:site_name' content='Morangesoft' />
+<meta name='twitter:title' content='Morangesoft' />
+<meta name='twitter:site' content='@Morangesoft' />
+<meta name='twitter:creator' content='@Morangesoft' />
+<meta name='geo.position' content='1123233' />
+<meta name='geo.placename' content='Place Name' />
+<meta name='geo.region' content='Country Subdivision Code' />
+<meta name='google-site-verification' content='dsasd4das454' />
+<meta charset='utf-8' />
+<meta name='DC.title' content='Morangesoft' />
+<meta name='og:description' content='Morangesoft' />
+<meta name='twitter:description' content='Morangesoft' />
+<meta name='description' content='Morangesoft' />
+<meta name='keywords' content='Morangesoft,dsa,dwwqd,dwqwdqdwq,dwq,wdqwdq,wdq' />
+?>
